cd1d1fb8adcdc9cd482ee8fb0674d164050195e8
KITI-loubou <919070296@qq.com>
2022-05-26 22:30:30 +0800

    update pick

--- questions/easy/pick/template.ts
+++ questions/easy/pick/template.ts
@@ -3,8 +3,8 @@
  * @version: 1.1
  * @Author: VLOU
  * @Date: 2022-05-14 17:58:34
- * @LastEditors: VLOU
- * @LastEditTime: 2022-05-14 19:10:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-26 22:28:39
  */
 
 /**
@@ -20,7 +20,7 @@
  * type Age = Person["age"];
  */
 type MyPick<T, K extends keyof T> = {
-  [P in K] : T[P]
+  [P in K]: T[P]
 }
 
 interface test {
@@ -28,11 +28,54 @@ interface test {
   age: number;
 }
 
-interface test2{
+interface test2 {
   name: string;
   sex: string;
 }
 
+interface returnType{
+  code: number;
+  data: object;
+  msg: string;
+}
+
+interface successType {
+  code: number;
+  data: object;
+}
+
+interface falseType {
+  code: string;
+  msg: string;
+}
+
+function test(r: returnType): any {
+  if (r.code === 0) {
+    let res: MyPick<returnType, keyof successType> = r
+    return res.data
+  }
+  let errorData: MyPick<returnType, keyof falseType> = r
+  if(errorData.code === 1){
+    return errorData.code
+  }
+}
+
+test({ code: 0, msg: '1', data: {} })
+
+// type Concrete<Type> = {
+//   [Property in keyof Type]-?: Type[Property];
+// };
+ 
+// type MaybeUser = {
+//   id: string;
+//   name?: string;
+//   age?: number;
+// };
+ 
+// type User = Concrete<MaybeUser>;
+
+// function test2(r: User): any {
+// }
 // sex不满足约束，不存在test中
 // type A = MyPick<test, 'sex'>
 
